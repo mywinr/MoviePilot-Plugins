69923d495dbd6fa347cd9081ca468cf023fb1fbe
mywinr <75130941+mywinr@users.noreply.github.com>
2025-09-20 10:56:41 +0800

    Update __init__.py

--- plugins/removelink/__init__.py
+++ plugins/removelink/__init__.py
@@ -196,7 +196,7 @@ class RemoveLink(_PluginBase):
     # 插件图标
     plugin_icon = "Ombi_A.png"
     # 插件版本
-    plugin_version = "2.5.1"
+    plugin_version = "2.5.2"
     # 插件作者
     plugin_author = "mywinr"
     # 作者主页
